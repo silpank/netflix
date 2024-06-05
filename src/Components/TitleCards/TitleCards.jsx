c85103779c1b36ba7cc967c21c56aeb85a4e1775
silpank <silpasadanandan08@gmail.com>
2024-06-05 22:46:09 +0530

    changes

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
-import cards_data from '../../assets/cards/Cards_data'
 import { Link } from 'react-router-dom';
 
 const TitleCards = ({title, category}) => {
