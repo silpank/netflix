c94fe452552736a380e4be77b24181ca0f13e381
silpank <silpasadanandan08@gmail.com>
2024-06-06 00:35:04 +0530

    new changes

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -2,47 +2,51 @@ import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import { Link } from 'react-router-dom';
 
-const TitleCards = ({title, category}) => {
+const TitleCards = ({ title, category }) => {
 
-  const [apiData, SetApiData] =useState([]);
-    const cardsRef =useRef();
-    
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNDc0NTM2YzE5MmM5M2Q5MzQ4MWRlOGZlMGExNzNjMSIsInN1YiI6IjY2NWFjNTM2YTNmYTc3ZmE1NjUyZGMyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YJL8pcmjp4R3lm4wJUC-_NE6cqAuCjZvAgnHvUe39a8'
-      }
-    };
-    
-    
+  const [apiData, SetApiData] = useState([]);
+  const cardsRef = useRef();
 
-const handleWheel=(event)=>{
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNDc0NTM2YzE5MmM5M2Q5MzQ4MWRlOGZlMGExNzNjMSIsInN1YiI6IjY2NWFjNTM2YTNmYTc3ZmE1NjUyZGMyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YJL8pcmjp4R3lm4wJUC-_NE6cqAuCjZvAgnHvUe39a8'
+    }
+  };
+
+  const handleWheel = (event) => {
     event.preventDefault();
-    cardsRef.current.scrollLeft +=event.deltax;
-}
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
 
-useEffect(()=>{
-
-  fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
       .then(response => response.json())
       .then(response => SetApiData(response.results))
       .catch(err => console.error(err));
 
-    cardsRef.current.addEventListener('wheel',handleWheel);},[])
+    const currentRef = cardsRef.current;
+    currentRef.addEventListener('wheel', handleWheel);
+
+    return () => {
+      currentRef.removeEventListener('wheel', handleWheel);
+    };
+  }, [category]);
+
   return (
     <div className='title-Cards'>
-        <h2>{title?title:"Popular on Netflix"}</h2>
-        <div className="card-list" ref={cardsRef}>
-         {apiData.map((card, index)=>{
-            return <Link to={`player/${card}`} className='card' key={index}>
-               <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" /> 
-              <p>{card.original_title}</p>
-            </Link>
-         })}   
-        </div>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData && apiData.map((card, index) => (
+          <Link to={`player/${card.id}`} className='card' key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`} alt={card.original_title} />
+            <p>{card.original_title}</p>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default TitleCards
+export default TitleCards;
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams();
+  const navigsate = useNavigate()
+
+const [apiData,setApiData]= useEffect({
+  name: "",
+  key:"",
+  published_at:"",
+  typeof:""
+})
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNDc0NTM2YzE5MmM5M2Q5MzQ4MWRlOGZlMGExNzNjMSIsInN1YiI6IjY2NWFjNTM2YTNmYTc3ZmE1NjUyZGMyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YJL8pcmjp4R3lm4wJUC-_NE6cqAuCjZvAgnHvUe39a8'
+    }
+  };
+
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+  
+  },[])
+
+
   return (
     <div className='player'>
-      <img src={back_arrow_icon} alt="" />
-      <iframe width='90%' height='90%' src='https://www.youtube.com/embed/KB_7X3iIHQ4' title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigsate(-2)}} />
+      <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder='0' allowFullScreen></iframe>
       <div className="player-info">
-        <p>Published Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
   )
